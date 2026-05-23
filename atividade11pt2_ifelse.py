43545635857cb06964184bf96f7fd8834898e918
kaingulosin <kaiocarmosiqueira@gmail.com>
2026-05-23 16:55:05 -0300

    Add files via upload

--- atividade11pt2_ifelse.py
+++ atividade11pt2_ifelse.py
@@ -1,10 +1,10 @@
-nome = input('digite seu nome')
-idade = input('digite sua idade: ')
-cnh = input('voce possui cnh sim ou não ')
+nome = input('digite seu nome: ')
+idade = int(input('digite sua idade: '))
+cnh = input('voce possui CNH sim ou não: ')
 if idade >= 18 :
     if cnh == 'sim':
         print(f'{nome} voce pode dirigir ')
     else:
-print(f'{nome} voce não pode dirigir')
+        print(f'{nome} voce NÃO pode dirigir')
 else:
     print(f'{nome} voce é menor de idade ')
